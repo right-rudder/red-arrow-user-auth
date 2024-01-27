c1720867f402083eaf05d2fd7f24482d02df9cd7
Tonie Santos <tonie_santos@yahoo.com>
2024-01-27 14:26:14 -0500

    fix redirects for non admin users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,14 @@ class HomeController < ApplicationController
     def index
         if !user_signed_in?
              redirect_to new_user_session_path # Redirect to the sign-in page if the user is not signed in
+        else 
+            # Rails.logger.info("**** Request referer " + request.referer + " document path " + documents_path)
+            ## this is for Red arrow only to redirect to Documents page 
+            if !current_user&.is_admin? && !request.referer.to_s.include?(documents_path)   
+                redirect_to documents_path
+            end
         end 
-        
+        ## the unapproved users is for Admin function. 
         @unapproved_users = User.where(is_approved: false)
         
     end
